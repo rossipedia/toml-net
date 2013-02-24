ce60706ba4fed27e105b75a457ec78adc60a68a2
Bryan Ross <bryanjamesross@gmail.com>
2013-02-24 02:38:30 -0700

    Initial Check In - Successfully parsed sample config

---
+++ toml-net.tests/ParseTests.cs
@@ -0,0 +1,213 @@
+﻿namespace Toml.Tests
+{
+    using System;
+    using System.Linq;
+
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class ParseTests
+    {
+        [Test]
+        public void NonNullStringShouldReturnNonNullConfig()
+        {
+            var config = string.Empty.ParseAsToml();
+            Assert.NotNull(config);
+        }
+
+        [Test]
+        public void ShouldParseKeyGroup()
+        {
+            var config = "[test]".ParseAsToml();
+
+            Assert.NotNull(config.test);
+        }
+
+        [Test]
+        public void ShouldParseNestedKeyGroup()
+        {
+            var config = "[foo.bar]".ParseAsToml();
+
+            Assert.NotNull(config.foo);
+            Assert.NotNull(config.foo.bar);
+        }
+
+        [Test]
+        public void ShouldParseMultipleKeyGroups()
+        {
+            var config = "[foo]\n[bar]".ParseAsToml();
+
+            Assert.NotNull(config.foo);
+            Assert.NotNull(config.bar);
+        }
+
+        [Test]
+        public void ShouldParseIntValue()
+        {
+            var config = "foo=1".ParseAsToml();
+
+            Assert.AreEqual(1, config.foo);
+        }
+
+        [Test]
+        public void ShouldParseFloatValue()
+        {
+            var config = "foo=1.23".ParseAsToml();
+
+            Assert.AreEqual(1.23, config.foo);
+        }
+
+        [Test]
+        public void ShouldParseTrueValue()
+        {
+            var config = "foo=true".ParseAsToml();
+
+            Assert.IsTrue(config.foo);
+        }
+
+        [Test]
+        public void ShouldParseFalseValue()
+        {
+            var config = "foo=false".ParseAsToml();
+
+            Assert.IsFalse(config.foo);
+        }
+
+        [Test]
+        public void ShouldParseSimpleStringValue()
+        {
+            var config = "foo=\"bar\"".ParseAsToml();
+
+            Assert.AreEqual("bar", config.foo);
+        }
+
+        [Test]
+        public void ShouldParseStringValueWithEscapedChars()
+        {
+            var config = "foo=\"Line1\\nLine2\\tFoo \\\"Bar\\\"\"".ParseAsToml();
+
+            Assert.AreEqual("Line1\nLine2\tFoo \"Bar\"", config.foo);
+        }
+
+        [Test]
+        public void ShouldParseZuluDateTime()
+        {
+            var config = "foo=2013-02-24T01:13:00Z".ParseAsToml();
+
+            Assert.AreEqual(new DateTime(2013, 02, 24, 01, 13, 00, DateTimeKind.Utc), config.foo);
+        }
+
+        [Test]
+        public void ShouldParseArrayOfInt()
+        {
+            var config = "foo=[1, 2, 3]".ParseAsToml();
+
+            Assert.IsTrue(new[] { 1, 2, 3 }.SequenceEqual((int[])config.foo));
+        }
+
+        [Test]
+        public void ShouldParseArrayOfFloat()
+        {
+            var config = "foo=[1.1, 2.2, 3.3]".ParseAsToml();
+
+            Assert.IsTrue(new[] { 1.1, 2.2, 3.3 }.SequenceEqual((double[])config.foo));
+        }
+
+        [Test]
+        public void ShouldParseArrayOfString()
+        {
+            var config = "foo=[\"foo\", \"bar\"]".ParseAsToml();
+
+            Assert.IsTrue(new[] { "foo", "bar" }.SequenceEqual((string[])config.foo));
+        }
+
+
+        [Test]
+        public void ShouldParseArrayOfBool()
+        {
+            var config = "foo=[true, false]".ParseAsToml();
+
+            Assert.IsTrue(new[] { true, false }.SequenceEqual((bool[])config.foo));
+        }
+
+        [Test]
+        public void ShouldParseArrayOfDateTime()
+        {
+            var config = "foo=[2013-02-24T01:13:00Z, 2013-01-30T22:30:15Z]".ParseAsToml();
+
+            var expected = new[]
+            {
+                new DateTime(2013, 02, 24, 01, 13, 00, DateTimeKind.Utc), 
+                new DateTime(2013, 01, 30, 22, 30, 15, DateTimeKind.Utc)
+            };
+            Assert.IsTrue(expected.SequenceEqual((DateTime[])config.foo));
+        }
+
+        [Test]
+        public void ShouldIgnoreComments()
+        {
+            var config = "#foo\n[bob]#bar".ParseAsToml();
+            Assert.NotNull(config);
+            Assert.NotNull(config.bob);
+        }
+
+        [Test]
+        public void ShouldParseSampleConfig()
+        {
+            const string SampleConfig =
+@"# This is a TOML document. Boom.
+
+title = ""TOML Example""
+
+[owner]
+name = ""Tom Preston-Werner""
+organization = ""GitHub""
+bio = ""GitHub Cofounder & CEO\nLikes tater tots and beer.""
+dob = 1979-05-27T07:32:00Z # First class dates? Why not?
+
+[database]
+server = ""192.168.1.1""
+ports = [ ""8001"", ""8001"", ""8002"" ]
+connection_max = 5000
+enabled = true
+
+[servers]
+
+  # You can indent as you please. Tabs or spaces. TOML don't care.
+  [servers.alpha]
+  ip = ""10.0.0.1""
+  dc = ""eqdc10""
+
+  [servers.beta]
+  ip = ""10.0.0.2""
+  dc = ""eqdc10""";
+
+            var config = SampleConfig.ParseAsToml();
+
+            Assert.NotNull(config);
+            Assert.AreEqual("TOML Example", config.title);
+
+            Assert.NotNull(config.owner);
+            Assert.AreEqual("Tom Preston-Werner", config.owner.name);
+            Assert.AreEqual("GitHub", config.owner.organization);
+            Assert.AreEqual("GitHub Cofounder & CEO\nLikes tater tots and beer.", config.owner.bio);
+            Assert.AreEqual(new DateTime(1979, 05, 27, 07, 32, 00, DateTimeKind.Utc), config.owner.dob);
+
+            Assert.NotNull(config.database);
+            Assert.AreEqual("192.168.1.1", config.database.server);
+            Assert.IsTrue(new[] { "8001", "8001", "8002" }.SequenceEqual((string[])config.database.ports));
+            Assert.AreEqual(5000, config.database.connection_max);
+            Assert.IsTrue((bool)config.database.enabled);
+
+            Assert.NotNull(config.servers);
+
+            Assert.NotNull(config.servers.alpha);
+            Assert.AreEqual("10.0.0.1", config.servers.alpha.ip);
+            Assert.AreEqual("eqdc10", config.servers.alpha.dc);
+
+            Assert.NotNull(config.servers.beta);
+            Assert.AreEqual("10.0.0.2", config.servers.beta.ip);
+            Assert.AreEqual("eqdc10", config.servers.beta.dc);
+        }
+    }
+}
---
+++ toml-net/Extensions.cs
@@ -0,0 +1,198 @@
+﻿namespace Toml
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+    using System.Dynamic;
+    using System.Globalization;
+    using System.IO;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    public static class Extensions
+    {
+        private static readonly Regex CommentExpression = new Regex(@"#.*$", RegexOptions.Compiled);
+        private static readonly Regex KeyGroupExpression = new Regex(@"^\[(\w+(\.\w+)*)]$", RegexOptions.Compiled);
+        private static readonly Regex ValueExpression = new Regex(@"^([^\s]+)\s*=(.+)$", RegexOptions.Compiled);
+
+        private static readonly Regex IntValueExpression = new Regex(@"^\d+$", RegexOptions.Compiled);
+        private static readonly Regex FloatValueExpression = new Regex(@"^\d+\.\d+?$", RegexOptions.Compiled);
+        private static readonly Regex BoolValueExpression = new Regex(@"^(true|false)$", RegexOptions.Compiled);
+        private static readonly Regex DateTimeValueExpression = new Regex(@"^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z", RegexOptions.Compiled);
+        private static readonly Regex StringValueExpression = new Regex(@"^"".*""$", RegexOptions.Compiled);
+        private static readonly Regex ArrayValueExpression = new Regex(@"^\[[^\]]+\]$", RegexOptions.Compiled);
+
+        private static readonly Dictionary<Regex, Func<string, object>> ParserMap = new Dictionary<Regex, Func<string, object>>
+        {
+            { IntValueExpression, s => int.Parse(s) },
+            { FloatValueExpression, s => double.Parse(s) },
+            { BoolValueExpression, s => bool.Parse(s) },
+            { DateTimeValueExpression, s => DateTime.ParseExact(
+                s, 
+                "yyyy-MM-ddTHH:mm:ssZ", 
+                CultureInfo.InvariantCulture, 
+                DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal) },
+            { StringValueExpression, ParseString },
+            { ArrayValueExpression, ParseArray }
+        };
+
+        public static dynamic ParseAsToml(this string str)
+        {
+            Contract.Requires(str != null);
+            using (var reader = new StringReader(str))
+            {
+                return Parse(reader);
+            }
+        }
+
+        private static dynamic Parse(TextReader reader)
+        {
+            var rootConfig = (IDictionary<string, object>)new ExpandoObject();
+            var current = rootConfig;
+            var currentPath = string.Empty;
+
+            var lineNo = 0;
+            while (true)
+            {
+                try
+                {
+                    var line = reader.ReadLine();
+                    lineNo++;
+                    if (line == null)
+                    {
+                        // Eof
+                        break;
+                    }
+
+                    line = StripWhitespaceAndComments(line);
+                    if (line == string.Empty)
+                    {
+                        continue; // We skip blank lines
+                    }
+
+                    var keyGroupMatch = KeyGroupExpression.Match(line);
+                    if (keyGroupMatch.Success)
+                    {
+                        var path = keyGroupMatch.Groups[1].Value;
+                        current = BuildKeyGroup(rootConfig, path);
+                        currentPath = path;
+                        continue;
+                    }
+
+                    var valueMatch = ValueExpression.Match(line);
+                    if (valueMatch.Success)
+                    {
+                        var key = valueMatch.Groups[1].Value;
+                        var value = valueMatch.Groups[2].Value.Trim();
+                        ThrowIfKeyExists(current, key, currentPath);
+                        current.Add(key, ParseValue(value));
+                        continue;
+                    }
+
+                    ThrowInvalidValueFound(line);
+                }
+                catch (FormatException ex)
+                {
+                    var message = string.Format("Error, Line: {0}, Message: {1}", lineNo, ex.Message);
+                    throw new FormatException(message, ex);
+                }
+            }
+
+            return rootConfig;
+        }
+
+        private static void ThrowIfKeyExists(IDictionary<string, object> current, string key, string currentPath)
+        {
+            if (!current.ContainsKey(key))
+            {
+                return;
+            }
+
+            var message = string.Format("Key {0} already exists under {1}", key, currentPath.Trim() == string.Empty ? "root" : "path " + currentPath);
+            throw new FormatException(message);
+        }
+
+        private static object ParseString(string value)
+        {
+            return value.First() == '"' && value.Last() == '"' && !value.Any(char.IsControl)
+                       ? Regex.Unescape(value.Substring(1, value.Length - 2))
+                       : null;
+        }
+
+        private static object ParseArray(string value)
+        {
+            // Simple for now (not-nested)
+            var values = value.Substring(1, value.Length - 2).Split(',').Select(s => s.Trim()).ToArray();
+            if (values.Length == 0)
+            {
+                return new object[0];
+            }
+
+            var parser = FindParserForValue(values[0]);
+            var objects = values.Select(s => s.Trim()).Select(parser).ToArray();
+
+            // Null means invalid parse
+            if (objects.Any(o => o == null))
+            {
+                return null;
+            }
+
+            // Determine type of objects
+            var type = objects[0].GetType();
+            var typedArray = Array.CreateInstance(type, objects.Length);
+            Array.Copy(objects, typedArray, objects.Length);
+            return typedArray;
+        }
+
+        private static object ParseValue(string value)
+        {
+            var parser = FindParserForValue(value);
+            if (parser == null)
+            {
+                ThrowInvalidValueFound(value);
+                return null; // Won't ever hit
+            }
+
+            return parser(value);
+        }
+
+        private static void ThrowInvalidValueFound(string value)
+        {
+            throw new FormatException(string.Format("Invalid Value: {0}", value));
+        }
+
+        private static Func<string, object> FindParserForValue(string value)
+        {
+            return ParserMap.Where(kv => kv.Key.IsMatch(value)).Select(kv => kv.Value).FirstOrDefault();
+        }
+
+        private static IDictionary<string, object> BuildKeyGroup(IDictionary<string, object> rootConfig, string keyGroupPath)
+        {
+            var current = rootConfig;
+            foreach (var part in keyGroupPath.Split('.'))
+            {
+                object existing;
+                if (current.TryGetValue(part, out existing) && !(existing is ExpandoObject))
+                {
+                    var message = string.Format("Key {0} has already been defined.", part);
+                    throw new FormatException(message);
+                }
+
+                if (existing == null)
+                {
+                    existing = new ExpandoObject();
+                    current.Add(part, existing);
+                }
+                
+                current = (ExpandoObject)existing;
+            }
+
+            return current;
+        }
+
+        private static string StripWhitespaceAndComments(string line)
+        {
+            return CommentExpression.Replace(line, string.Empty).Trim();
+        }
+    }
+}
